41dc4de4fac3df6903dcab2df2430a321dad1fe3
Yos1f <giuseppe@giuseppecatalano.com>
2022-08-09 00:11:56 +0000

    more tables, pm2 script

--- Config.js
+++ Config.js
@@ -4,7 +4,8 @@ class Config {
         this.sleepMax = 2000;
         // this.wsServer = "ws://YX1:8111/ws";
         // this.wsServer = "ws://localhost:8111/ws";
-        this.wsServer = "ws://pokerclient.polpoker.com/ws"
+        //this.wsServer = "ws://pokerclient.polpoker.com/ws"
+	this.wsServer = "ws://10.128.15.199:8111/ws"
         //this.wsServer = "ws://34.89.40.196/ws";
         // this.wsServer = "ws://cryptopoker.fun/ws";
         this.maxSeat = 7;
